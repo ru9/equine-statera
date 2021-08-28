7523dc120ba2801471a9c2e9cee6ce4bcfd8b20f
ru9 <runisunde@gmail.com>
2021-08-28 22:34:18 +0200

    Redone responsive elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,40 +2,41 @@
   <v-container fill-height align-start>
     <h1>Equine Statera</h1>
     <v-container>
-      <v-row>
-        <v-row>
-          <v-col>
-            <p class="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum
-              distinctio quia maiores nam placeat ea facere voluptates
-              temporibus quibusdam. Sint quos, modi cupiditate sequi debitis
-              reprehenderit doloremque fugit beatae!
-            </p>
-          </v-col>
-        </v-row>
+      <v-row justify="center">
+        <v-col cols="12" :sm="11" :md="10" :lg="7" :xl="5">
+          <p class="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eum
+            distinctio quia maiores nam placeat ea facere voluptates temporibus
+            quibusdam. Sint quos, modi cupiditate sequi debitis reprehenderit
+            doloremque fugit beatae!
+          </p>
+        </v-col>
+      </v-row>
 
-        <v-row justify="center">
-          <v-col cols="12">
-            <v-text-field label="Your E-mail address" filled rounded dense>
-            </v-text-field>
-          </v-col>
+      <v-row class="mb-10" justify="center">
+        <v-col cols="10" :sm="8" :md="8" :lg="6" :xl="4">
+          <v-text-field label="Your E-mail address" filled rounded dense>
+          </v-text-field>
+        </v-col>
 
-          <v-col cols="6">
-            <v-btn x-large rounded depressed> Subscribe </v-btn>
-          </v-col>
-        </v-row>
+        <v-btn class="mt-2 d-flex d-sm-none" depressed fab>
+          <v-icon dark>mdi-plus</v-icon>
+        </v-btn>
+        <v-btn class="mt-3 d-none d-sm-flex" x-large rounded depressed
+          >Subscribe</v-btn
+        >
+      </v-row>
 
-        <v-row justify="center">
-          <v-btn
-            color="blue lighten-3"
-            x-large
-            rounded
-            depressed
-            to="/horseexercises"
-          >
-            Horse Exercises
-          </v-btn>
-        </v-row>
+      <v-row justify="center">
+        <v-btn
+          color="blue lighten-3"
+          x-large
+          rounded
+          depressed
+          to="/horseexercises"
+        >
+          Horse Exercises
+        </v-btn>
       </v-row>
     </v-container>
   </v-container>
